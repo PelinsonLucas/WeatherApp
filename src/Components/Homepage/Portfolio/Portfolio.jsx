cb67f2d94b48ee09cb888a6b09b48cce20ddaaf5
PelinsonLucas <46092675+PelinsonLucas@users.noreply.github.com>
2023-11-13 02:15:09 -0300

    Update Portfolio.jsx

--- src/Components/Homepage/Portfolio/Portfolio.jsx
+++ src/Components/Homepage/Portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Portfolio.css'; // Assuming you have a corresponding CSS file
+import { Link } from 'react-router-dom';
 
 import screenshot1 from '../../Assets/weatherapp.png'; // Replace with your screenshot path
 
@@ -8,10 +9,12 @@ const Portfolio = () => {
     <div id="portfolio" className="portfolio-section">
       <h2 className="section-title animated">My Projects</h2>
       <div className="projects animated">
-        <a href="/weather" className="project">
+        <a className="project">
+          <Link to = "/weather">
           <h3 className="project-title">Project 1</h3>
           <p className="project-description">This is a brief description of Project 1.</p>
           <img src={screenshot1} alt="Project 1 Screenshot" className="project-screenshot" />
+          </Link>
         </a>
         {/* Add more projects as needed */}
       </div>
