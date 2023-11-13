4e46c76e5bdc6953db244c6ab6365691032d3dca
PelinsonLucas <46092675+PelinsonLucas@users.noreply.github.com>
2023-11-12 23:59:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import Homepage from './Components/Homepage/Homepage';
 import WeatherApp from './Components/WeatherApp/WeatherApp';
 import Header from './Header/Header.jsx';
-import { Routes, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Routes>
+      <Switch>
+        <Header/>
         <Route path="/weather" element={<WeatherApp />} />
         <Route path="/" element={<Homepage />} />
-      </Routes>
-      <Header/>
+      </Switch>
     </>
   );
 }
