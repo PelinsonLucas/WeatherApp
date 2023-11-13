7ce7a5a28c6198ac751c7f0cd038877418a59dff
PelinsonLucas <46092675+PelinsonLucas@users.noreply.github.com>
2023-11-12 23:00:39 -0300

    update

--- portfolio/src/Components/Homepage/Home/Home.jsx
+++ portfolio/src/Components/Homepage/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div id="home" className="home-section">
       <h1 className="greeting animated">Hello, I'm Lucas Pelinson</h1>
-      <p className="introduction animated">I'm a full-stack web developer. I specialize in...</p>
+      <p className="introduction animated">I'm a Software Developer. </p>
       <img src={profile} alt="Profile" className="profile-picture animated"/> {/* Replace with your image path */}
       {/* Add more elements as needed */}
     </div>
